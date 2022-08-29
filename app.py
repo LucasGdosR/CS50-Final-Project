af6043de490d4e946b92a3ff9c3b72d68262a08c
LucasGdosReis <lucas.gribel.reis@gmail.com>
2022-08-29 13:24:37 -0300

    db working with 'exact'. only 'min' method to go

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from cs50 import SQL
 from flask import Flask, flash, render_template, request
 import numpy as np
+import sqlite3
 
 app = Flask(__name__)
 
-db = SQL("sqlite///nutritionfacts.db")
+con = sqlite3.connect("nutritionfacts.db", check_same_thread=False)
+cur = con.cursor()
 
 
 @app.route("/", methods=['GET', 'POST'])
@@ -17,33 +18,36 @@ def index():
         ingredient1 = request.form.get('db1')
         ingredient2 = request.form.get('db2')
         ingredient3 = request.form.get('db3')
-        macros1 = db.execute('SELECT carbohydrate, protein, fat FROM nutritionfacts WHERE name == ?', ingredient1)
-        macros2 = db.execute('SELECT carbohydrate, protein, fat FROM nutritionfacts WHERE name == ?', ingredient2)
-        macros3 = db.execute('SELECT carbohydrate, protein, fat FROM nutritionfacts WHERE name == ?', ingredient3)
-        carbs = int(request.form.get('carbs'))
-        protein = int(request.form.get('protein'))
-        fats = int(request.form.get('fats'))
+        res = cur.execute('SELECT carbohydrate, protein, fat FROM nutritionfacts WHERE name = ?', (ingredient1,))
+        macros1 = res.fetchall()
+        res = cur.execute('SELECT carbohydrate, protein, fat FROM nutritionfacts WHERE name = ?', (ingredient2,))
+        macros2 = res.fetchall()
+        res = cur.execute('SELECT carbohydrate, protein, fat FROM nutritionfacts WHERE name = ?', (ingredient3,))
+        macros3 = res.fetchall()
+        carbstarget = int(request.form.get('carbs'))
+        proteintarget = int(request.form.get('protein'))
+        fatstarget = int(request.form.get('fats'))
         # carbs = a*m1.carbs + b*m2.carbs + c*m3 carbs
         # protein = a*m1.protein + b*m2.protein + c*m3 protein
         # fats = a*m1.fats + b*m2.fats + c*m3 fats
-        A = np.matrix([[macros1['carbohydrate'],macros2['carbohydrate'],macros3['carbohydrate']],
-                       [macros1['protein'],macros2['protein'],macros3['protein']],
-                       [macros1['fat'],macros2['fat'],macros3['fat']]])
-        B = np.matrix([[carbs],[protein],[fats]])
+        A = np.matrix([[float(macros1[0][0]),float(macros2[0][0]),float(macros3[0][0])],
+                       [float(macros1[0][1]),float(macros2[0][1]),float(macros3[0][1])],
+                       [float(macros1[0][2]),float(macros2[0][2]),float(macros3[0][2])]])
+        B = np.matrix([[carbstarget],[proteintarget],[fatstarget]])
         A_inverse = np.linalg.inv(A)
         X = A_inverse * B
-        for i in X:
-            if X[i] < 0:
+        for i in range(3):
+            if X[i][0] < 0:
                 return('negative.html')
-            X[i] = round(X[i] * 100)
-        return render_template('dbexact.html', X)
+            X[i][0] = round(float(X[i][0] * 100))
+        return render_template('dbexact.html', X=X, carbstarget=carbstarget, proteintarget=proteintarget, fatstarget=fatstarget, ingredient1=ingredient1, ingredient2=ingredient2, ingredient3=ingredient3)
     elif request.form.get('exactminchoice') == 'min' and request.form.get('dbcustomchoice') == 'db':
         ingredient1 = request.form.get('db1')
         ingredient2 = request.form.get('db2')
         ingredient3 = request.form.get('db3')
-#        macros1 = db.execute('SELECT calories, protein, fats FROM nutritionfacts WHERE food == ?', ingredient1)
-#        macros2 = db.execute('SELECT calories, protein, fats FROM nutritionfacts WHERE food == ?', ingredient2)
-#        macros3 = db.execute('SELECT calories, protein, fats FROM nutritionfacts WHERE food == ?', ingredient3)
+#        macros1 = db.execute('SELECT calories, protein, fat FROM nutritionfacts WHERE food == ?', ingredient1)
+#        macros2 = db.execute('SELECT calories, protein, fat FROM nutritionfacts WHERE food == ?', ingredient2)
+#        macros3 = db.execute('SELECT calories, protein, fat FROM nutritionfacts WHERE food == ?', ingredient3)
         calories = int(request.form.get('calories'))
         protein = int(request.form.get('protein'))
         fats = int(request.form.get('fats'))
@@ -83,7 +87,7 @@ def index():
         for i in range(3):
             if X[i] < 0:
                 return('negative.html')
-            X[i] = round(int(X[i] * weight[i]))
+            X[i] = round(float(X[i] * weight[i]))
         return render_template('customexact.html', X=X, carbstarget=carbstarget, proteintarget=proteintarget, fatstarget=fatstarget)
     elif request.form.get('exactminchoice') == 'min' and request.form.get('dbcustomchoice') == 'custom':
         return render_template('custommin.html')
