6253f2d2e30d3f0d79922b25bc07b34c1cc75fe0
LucasGdosReis <lucas.gribel.reis@gmail.com>
2022-08-30 14:29:26 -0300

    Min DB option working with scipy. Added comments

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
+from ast import Return
 from flask import Flask, flash, render_template, request
 import numpy as np
+from scipy.optimize import linprog
 import sqlite3
 
 app = Flask(__name__)
@@ -13,7 +15,10 @@ def index():
     # First access or return after result
     if request.method == 'GET' or request.form.get('return') == True:
         return render_template("index.html")
+
     # Use a multiple options input to determine which page to render next
+    # Exact macros using database
+    # DONE
     elif request.form.get('exactminchoice') == 'exact' and request.form.get('dbcustomchoice') == 'db':
         ingredient1 = request.form.get('db1')
         ingredient2 = request.form.get('db2')
@@ -41,34 +46,41 @@ def index():
                 return('negative.html')
             X[i][0] = round(float(X[i][0] * 100))
         return render_template('dbexact.html', X=X, carbstarget=carbstarget, proteintarget=proteintarget, fatstarget=fatstarget, ingredient1=ingredient1, ingredient2=ingredient2, ingredient3=ingredient3)
+    
+    # Calorie target using database
     elif request.form.get('exactminchoice') == 'min' and request.form.get('dbcustomchoice') == 'db':
         ingredient1 = request.form.get('db1')
         ingredient2 = request.form.get('db2')
         ingredient3 = request.form.get('db3')
-#        macros1 = db.execute('SELECT calories, protein, fat FROM nutritionfacts WHERE food == ?', ingredient1)
-#        macros2 = db.execute('SELECT calories, protein, fat FROM nutritionfacts WHERE food == ?', ingredient2)
-#        macros3 = db.execute('SELECT calories, protein, fat FROM nutritionfacts WHERE food == ?', ingredient3)
-        calories = int(request.form.get('calories'))
-        protein = int(request.form.get('protein'))
-        fats = int(request.form.get('fats'))
+        res = cur.execute('SELECT kcal, prot, fat FROM taco WHERE name = ?', (ingredient1,))
+        macros1 = res.fetchall()
+        res = cur.execute('SELECT kcal, prot, fat FROM taco WHERE name = ?', (ingredient2,))
+        macros2 = res.fetchall()
+        res = cur.execute('SELECT kcal, prot, fat FROM taco WHERE name = ?', (ingredient3,))
+        macros3 = res.fetchall()
+        caloriestarget = int(request.form.get('calories'))
+        proteintarget = int(request.form.get('protein'))
+        fatstarget = int(request.form.get('fats'))
         # https://stackoverflow.com/questions/48966934/solve-a-system-of-linear-equations-and-linear-inequalities
-        # Tenho que encontrar uma região em que as inequações são atendidas, determinar o ponto em que peso1 é máximo, e encontrar peso2 e peso3 correspondentes
         # P >= PT, F >= FT, kcal = kcalT, pesos >= 0, peso1 max
-        #
-        # carbs = a*m1.carbs + b*m2.carbs + c*m3 carbs
-        # protein = a*m1.protein + b*m2.protein + c*m3 protein
-        # fats = a*m1.fats + b*m2.fats + c*m3 fats
-        #A = np.matrix([[macros1['calories'],macros2['calories'],macros3['calories']],
-        #               [macros1['protein'],macros2['protein'],macros3['protein']],
-        #               [macros1['fats'],macros2['fats'],macros3['fats']]])
-        #B = np.matrix([[calories],[protein],[fats]])
-        #A_inverse = np.linalg.inv(A)
-        #X = A_inverse * B
-        #for i in X:
-        #    if X[i] < 0:
-        #        return('negative.html')
-        #    X[i] = round(X[i] * 100)
-        return render_template('dbmin.html', X)
+        coefficients_inequalities = [[-float(macros1[0][1]), -float(macros2[0][1]), -float(macros3[0][1])], [-float(macros1[0][2]), -float(macros2[0][2]), -float(macros3[0][2])]]  # require -1*x + -1*y <= -180
+        constants_inequalities = [-proteintarget, -fatstarget]
+        coefficients_equalities = [[float(macros1[0][0]), float(macros2[0][0]), float(macros3[0][0])]]
+        constants_equalities = [caloriestarget]
+        coefficients_max_1 = [-1, 0, 0]
+        res = linprog(coefficients_max_1,
+              A_ub=coefficients_inequalities,
+              b_ub=constants_inequalities,
+              A_eq=coefficients_equalities,
+              b_eq=constants_equalities, # bounds=((0, None), (0, None), (0, None))
+        )
+        #print('Maximum value of y =', -res.fun)  # opposite of value of -y
+        if res.success == False:
+            return render_template('negative.html')
+        return render_template('dbmin.html', weight1=round(res.x[0] * 100), weight2=round(res.x[1] * 100), weight3=round(res.x[2] * 100), caloriestarget=caloriestarget, proteintarget=proteintarget, fatstarget=fatstarget, ingredient1=ingredient1, ingredient2=ingredient2, ingredient3=ingredient3)
+    
+    # Exact macros with custom foods
+    # DONE
     elif request.form.get('exactminchoice') == 'exact' and request.form.get('dbcustomchoice') == 'custom':
         weight = [int(request.form.get('weight1')), int(request.form.get('weight2')), int(request.form.get('weight3'))] 
         carbs = [int(request.form.get('carbs1')), int(request.form.get('carbs2')), int(request.form.get('carbs3'))]
@@ -89,7 +101,11 @@ def index():
                 return('negative.html')
             X[i] = round(float(X[i] * weight[i]))
         return render_template('customexact.html', X=X, carbstarget=carbstarget, proteintarget=proteintarget, fatstarget=fatstarget)
+    
+    # Calorie target with custom foods
     elif request.form.get('exactminchoice') == 'min' and request.form.get('dbcustomchoice') == 'custom':
         return render_template('custommin.html')
+
+    # If the options aren't selected
     else:
         return flash('Invalid input. Make sure to select an option: 1) database or custom foods; and 2) exact macros or minimum targets')
